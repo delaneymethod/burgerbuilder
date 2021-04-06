c6518e2f9ae501f3b72423115238c4b54a331adb
Sean Delaney <hello@delaneymethod.com>
2021-04-06 16:22:31 +0100

    Basic burger builder
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import classes from './Burger.module.css';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+class Burger extends Component {
+	render() {
+		let transformedIngredients = Object
+			.keys(this.props.ingredients)
+			.map(ingredientKey => {
+				return [...Array(this.props.ingredients[ingredientKey])].map((_, index) => {
+					return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
+				});
+			})
+			.reduce((array, element) => {
+				return array.concat(element);
+			}, []);
+
+		if (transformedIngredients.length === 0) {
+			transformedIngredients = <p>Please start adding ingredients</p>
+		}
+
+		console.log('[Burger.js] transformedIngredients', transformedIngredients);
+
+		return (
+			<div className={classes.Burger}>
+				<BurgerIngredient type={'bread-top'} />
+				{transformedIngredients}
+				<BurgerIngredient type={'bread-bottom'} />
+			</div>
+		);
+	};
+}
+
+export default Burger;
