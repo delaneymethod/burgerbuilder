655c593c9b762d34ea84e89750d2c2fa8760cdc4
Sean Delaney <hello@delaneymethod.com>
2021-04-13 13:40:02 +0100

    Folder restructure
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/store/reducers/authenticate.js
+++ src/store/reducers/authenticate.js
@@ -1,4 +1,4 @@
-import { updateObject } from '../utility';
+import { updateObject } from '../../shared/utility';
 import {
 	AUTHENTICATE_SUCCESS,
 	AUTHENTICATE_FAIL,
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,4 @@
-import { updateObject } from '../utility';
+import { updateObject } from '../../shared/utility';
 import {
 	FETCH_ORDERS_FAIL,
 	FETCH_ORDERS_START,
