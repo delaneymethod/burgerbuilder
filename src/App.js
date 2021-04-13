6a2fcb7c0f0f834790de2cb96fdd29c0618e94c3
Sean Delaney <hello@delaneymethod.com>
2021-04-13 13:19:18 +0100

    Simple guards
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { authenticateAuto } from './store/actions';
@@ -19,19 +19,43 @@ class App extends Component {
 	 * @returns {JSX.Element}
 	 */
 	render() {
-		return (
-			<Layout>
+		let routes = (
+			<Switch>
+				<Route path={'/authenticate'} component={Authenticate}/>
+				<Route path={'/'} exact component={BurgerBuilder}/>
+				<Redirect to={'/'}/>
+			</Switch>
+		);
+
+		if (this.props.authenticated) {
+			routes = (
 				<Switch>
 					<Route path={'/checkout'} component={Checkout}/>
 					<Route path={'/orders'} component={Orders}/>
-					<Route path={'/authenticate'} component={Authenticate}/>
 					<Route path={'/logout'} component={Logout}/>
 					<Route path={'/'} exact component={BurgerBuilder}/>
+					<Redirect to={'/'}/>
 				</Switch>
+			);
+		}
+
+		return (
+			<Layout>
+				{routes}
 			</Layout>
 		);
 	};
 }
+
+/**
+ * @param state
+ * @returns {{authenticated: boolean}}
+ */
+const mapStateToProps = state => {
+	return {
+		authenticated: state.authenticate.idToken !== null
+	};
+};
 
 /**
  * @param dispatch
@@ -43,4 +67,4 @@ const mapDispatchToProps = dispatch => {
 	};
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
